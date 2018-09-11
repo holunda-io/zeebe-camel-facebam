c7e14ef4cae4a765fc9465f27c782568be0d8d76
Jan Galinski <jan.galinski@holisticon.de>
2018-09-11 21:42:16 +0200

    broker with client app, fixes #1

---
+++ buildSrc/src/main/kotlin/Build.kt
@@ -0,0 +1,14 @@
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.version
+import org.gradle.plugin.use.PluginDependenciesSpec
+import org.gradle.plugin.use.PluginDependencySpec
+
+object Versions {
+  val kotlin = "1.2.61"
+  val dependencyManagement = "1.0.6.RELEASE"
+  val springBoot = "2.0.4.RELEASE"
+  val zeebe = "0.11.0"
+  val camel = "2.22.0"
+  val springZeebe = "0.3.0-SNAPSHOT"
+  val camelZeebe = "0.0.1-SNAPSHOT"
+}
